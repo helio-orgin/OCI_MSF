672ed1f772037ecb8128d5ecba05e0422bf6f7a7
helio-orgin <84082521+helio-orgin@users.noreply.github.com>
2021-09-01 18:19:21 +0530

    Add files via upload

---
+++ OCI_MSF.py
@@ -0,0 +1,26 @@
+import os
+
+os.system("pkg install figlet -y")
+os.system("pkg install wget -y")
+os.system("figlet -f big OCI")
+print("Downloading Termux Black.... ")
+os.system("wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh")
+os.system("chmod +x install.sh")
+os.system("bash install.sh -i")
+print("Now we are going install Metaspliot. It takes mostly 5 to 15 min based on your internet speed")
+# choice = input("You need 500 mb data to install Metasploit y/n:")
+yes = ["y", "Y"]
+no = ["n" ,"N"]
+while True:
+    choice = input("You need 500 mb data to install Metasploit y/n:")
+    if choice in yes:
+        os.system("git clone https://github.com/helio-orgin/OCI_MSF/blob/main/metasploit.sh")
+        os.system("chmod +x metasploit.sh")
+        os.system("./metasploit.sh")
+        print("Thanks for using our tool ---->  Origin Cyber Sec")
+        break
+    elif choice in no:
+        print("Try this tool when you have sufficient data. Thankyou for using our tool")
+        break
+    else:
+        print("Invalid input, try again")
